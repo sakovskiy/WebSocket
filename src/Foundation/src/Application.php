5f7d67a0c9e8ec4ee8e35a9f23623d0820eb7c0f
Sergiy Sakovskiy <sakovskiy.sergiy@gmail.com>
2018-01-12 14:37:11 +0200

    init 2

---
+++ src/Foundation/src/Application.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 12.01.2018
+ * Time: 14:22
+ */
+
+namespace Lara\Fundation;
+
+
+/**
+ * Class Application
+ * @package Lara\Fundation
+ */
+class Application
+{
+
+}
